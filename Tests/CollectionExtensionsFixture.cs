71330e964a77a5205a33170d0b84bfa7c31f59f2
Chris McKelt <chris@mckelt.com>
2015-05-11 13:06:26 +0800

    start adding missing tests

--- Tests/CollectionExtensionsFixture.cs
+++ Tests/CollectionExtensionsFixture.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using FizzWare.NBuilder;
 using Xunit;
 
@@ -21,6 +22,90 @@ namespace ExtensionMinder.Tests
 
             Assert.True(list.All(a=>a.IntegerProperty==5));
 
+        }
+
+        [Fact]
+        public void MaxByWillSelectMax()
+        {
+            const int i = 1;
+
+            var list = Builder<TestObject>
+                .CreateListOfSize(3)
+                .All()
+                .With(x => x.IntegerProperty = i + 1)
+                .Build();
+
+            var max = list.MaxBy<TestObject, int>(a => a.IntegerProperty);
+
+            Assert.Equal(list.Last().IntegerProperty, max.IntegerProperty);
+
+        }
+
+        [Fact]
+        public void MaxByWillSelectMaxWithSpecifiedComparer()
+        {
+            const int i = 1;
+
+            var list = Builder<TestObject>
+                .CreateListOfSize(3)
+                .All()
+                .With(x => x.IntegerProperty = i + 1)
+                .Build();
+
+            var max = list.MaxBy<TestObject, int>(a => a.IntegerProperty, Comparer<int>.Default);
+
+            Assert.Equal(list.Last().IntegerProperty, max.IntegerProperty);
+
+        }
+
+        [Fact]
+        public void MapWorks()
+        {
+            const int i = 1;
+
+            var list = Builder<TestObject>
+                .CreateListOfSize(3)
+                .All()
+                .With(x => x.IntegerProperty = i + 1)
+                .Build();
+
+            var result = list.Map<TestObject>(a =>
+            {
+                var x = a;
+                x.IntegerProperty = 44;
+                return x;
+            });
+
+            result.All(a => a.IntegerProperty == 44);
+        }
+
+        [Fact]
+        public void GetDuplicatesWorks()
+        {
+            const int i = 1;
+
+            var list = Builder<TestObject>
+                .CreateListOfSize(3)
+                .TheFirst(2)
+                .With(x => x.IntegerProperty = i)
+                .All()
+                .With(x=>x.IntegerProperty = i+1)
+                .Build();
+
+            var duplicateList = new List<TestObject>();
+
+            foreach (var testObject in list)
+            {
+                duplicateList.Add(testObject);
+                if (testObject.IntegerProperty == i)
+                {
+                    duplicateList.Add(testObject);
+                }
+            }
+
+            Assert.Equal(2, duplicateList.GetDuplicates().Count());
+            
+           
         }
     }
 }
