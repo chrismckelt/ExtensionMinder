05cd5b743e6512f3798a09237638bff558389642
Chris McKelt <chris@mckelt.com>
2015-05-12 09:03:12 +0800

    breaking the build

---
+++ Tests/EnumExtensionsTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using Phoenix.Core.Domain.Accounts;
+using Phoenix.Core.Domain.Products.Payments;
+using Phoenix.Core.Libraries.Extensions;
+using Should;
+using Xunit;
+
+namespace Phoenix.Core.UnitTests.Libraries.Extensions
+{
+    public class EnumExtensionsTestFixture
+    {
+        [Fact]
+        public void GetDescription_ReturnsCorrectDescription()
+        {
+            var x = AccountBankType.AutoCash;
+            var str = x.GetDescription();
+            str.ShouldEqual("Auto Cash");
+        }
+
+        [Fact]
+        public void StringToEnum_ShouldConvertStringToEnum()
+        {
+            var str = "AutoCash";
+            var x = str.ToEnum<AccountBankType>();
+            x.ShouldEqual(AccountBankType.AutoCash);
+        }
+
+        [Fact]
+        public void GetEnumFromValue_ReturnsCorrectEnum()
+        {
+            double value = 12;
+            var result = value.GetEnumFromValue<PaymentFrequency>();
+            double value2 = 1;
+            var result2 = value2.GetEnumFromValue<PaymentFrequency>();
+
+            result.ShouldEqual(PaymentFrequency.Monthly);
+            result2.ShouldEqual(PaymentFrequency.Yearly);
+        }
+    }
+}
---
+++ Tests/ExtensionsTestFixture.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Phoenix.Core.Libraries.Extensions;
+using Should;
+using Xunit;
+
+namespace Phoenix.Core.UnitTests.Libraries.Extensions
+{
+
+    public class ExtensionsTestFixture
+    {
+
+        [Fact]
+        public void TrimToLengthShouldCutStringToSpecfiedLength()
+        {
+            string s = "123456";
+            string x = s.TrimToLength(3);
+            x.Length.ShouldEqual(3);
+        }
+
+        [Fact]
+        public void CalculateAgeCalculatesAgeCorrectlyWithDobLaterInYear()
+        {
+            var dob = DateTime.Now.AddDays(1).AddYears(-10);
+            var age = dob.CalculateAge();
+            age.ShouldEqual(9);
+        }
+
+        [Fact]
+        public void CalculateAgeCalculatesAgeCorrectlyWithDobEarlierInYear()
+        {
+            var dob = DateTime.Now.AddDays(-1).AddYears(-10);
+            var age = dob.CalculateAge();
+            age.ShouldEqual(10);
+        }
+
+        [Fact]
+        public void ShowIf_ReturnsStringWhenFuncIsTrue()
+        {
+            var result = "This".ShowIf(() => true);
+
+            result.ShouldEqual("This");
+        }
+
+        [Fact]
+        public void ShowIf_ReturnsEmptyStringWhenFuncIsFalse()
+        {
+            var result = "This".ShowIf(() => false);
+
+            result.ShouldEqual("");
+        }
+    }
+}
