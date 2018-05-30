98cb758ef705a32b29a8bfe464f68b59886be88b
Chris McKelt <chris@mckelt.com>
2018-05-30 21:42:27 +0800

    ToAlphaOnly ToAlphaNumericOnly ToAlphaNumericOnly (#12)

--- ExtensionMinder.Tests/StringExtensionShould.cs
+++ ExtensionMinder.Tests/StringExtensionShould.cs
@@ -5,34 +5,53 @@ using Xunit;
 
 namespace ExtensionMinder.Tests
 {
-    public class StringExtensionShould
+  public class StringExtensionShould
+  {
+    [Fact]
+    public void TrimAllStrings_trims_all_strings()
     {
-      [Fact]
-      public void TrimAllStrings_trims_all_strings()
-      {
-        var to = new TestObject {StringProperty = "   hello   "};
-        var result = to.TrimAllStrings();
-        Assert.Equal("hello", result.StringProperty);
-      }
-
-      [Fact]
-      public void SplitSentenceIntoWords_removes_junk()
-      {
-        const string test = "hell this ain't the best! words to split 123";
-        var words = test.SplitSentenceIntoWords();
-        foreach (var word in words)
-        {
-          Console.WriteLine(word);
-        }
-
-        words.Count().Should().Be(9);
-      }
-
-      [Fact]
-      public void Clean_removes_dodgy_characters()
-      {
-        string text = @"abcd&efg-h";
-        text.Clean().Should().Be("abcdefgh");
-      }
+      var to = new TestObject {StringProperty = "   hello   "};
+      var result = to.TrimAllStrings();
+      Assert.Equal("hello", result.StringProperty);
     }
+
+    [Fact]
+    public void SplitSentenceIntoWords_removes_junk()
+    {
+      const string test = "hell this ain't the best! words to split 123";
+      var words = test.SplitSentenceIntoWords();
+      foreach (var word in words) Console.WriteLine(word);
+
+      words.Count().Should().Be(9);
+    }
+
+    [Fact]
+    public void Clean_removes_dodgy_characters()
+    {
+      var text = @"abcd&efg-h";
+      text.Clean().Should().Be("abcdefgh");
+    }
+
+    [Fact]
+    public void ToAlphaNumericOnly()
+    {
+      var text = @"abc123!@#";
+      text.ToAlphaNumericOnly().Should().Be("abc123");
+    }
+
+    [Fact]
+    public void ToAlphaOnly()
+    {
+      var text = @"abc123!@#";
+      text.ToAlphaOnly().Should().Be("abc");
+    }
+
+
+    [Fact]
+    public void ToNumericOnly()
+    {
+      var text = @"abc123!@#";
+      text.ToNumericOnly().Should().Be("123");
+    }
+  }
 }
