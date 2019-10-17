115693f66387fead9b7f74630d7ba45c11ee7193
Chris McKelt <chris@mckelt.com>
2019-10-17 18:04:12 +0800

    ToCsv

--- ExtensionMinder/CollectionExtensions.cs
+++ ExtensionMinder/CollectionExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 
 namespace ExtensionMinder
@@ -138,6 +139,41 @@ namespace ExtensionMinder
                     yield return result;
                 }
             }
+        }
+
+        public static string ToCsv<T>(this IEnumerable<T> items, char separator)
+        {
+            var output = "";
+            var delimiter = ',';
+            var properties = typeof(T).GetProperties()
+                .Where(n =>
+                    n.PropertyType == typeof(string)
+                    || n.PropertyType == typeof(bool)
+                    || n.PropertyType == typeof(char)
+                    || n.PropertyType == typeof(byte)
+                    || n.PropertyType == typeof(decimal)
+                    || n.PropertyType == typeof(int)
+                    || n.PropertyType == typeof(DateTime)
+                    || n.PropertyType == typeof(DateTime?));
+            using (var sw = new StringWriter())
+            {
+                var header = properties
+                    .Select(n => n.Name)
+                    .Aggregate((a, b) => a + delimiter + b);
+                sw.WriteLine(header);
+                foreach (var item in items)
+                {
+                    var row = properties
+                        .Select(n => n.GetValue(item, null))
+                        .Select(n => n?.ToString())
+                        .Aggregate((a, b) => a + delimiter + b);
+                    sw.WriteLine(row);
+                }
+
+                output = sw.ToString();
+            }
+
+            return output;
         }
     }
 }
